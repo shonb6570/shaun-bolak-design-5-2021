e6afc9ca534ffa5b521c0ce2870be75888eda25a
Shaun Bolak <47017803+shonb6570@users.noreply.github.com>
2021-05-28 17:16:09 -0400

    initial commit

---
+++ shaun-bolak-design-v2/src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h2 className="h2-title">hello</h2>
+    </div>
+  );
+}
+
+export default App;
